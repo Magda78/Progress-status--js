2431d3f464295b2076f61554d23bec4759e895e2
Magda <>
2021-02-28 18:27:57 -0500

    connecting line

--- index.js
+++ index.js
@@ -2,28 +2,43 @@ const next = document.getElementById('next');
 const prev = document.getElementById('prev');
 const numbers = document.querySelectorAll('.numbers');
 const line = document.getElementById('line');
+const progress = document.querySelector('.progress');
+const active = document.querySelectorAll('.active');
 
-let idx = 1
-
+let element = 1;
 
 const nextHandler = () => {
-const element = numbers[idx];
-console.log(idx)
-element.classList.add('active')
-idx++;
-console.log(element)   
-}
+	element ++;
+	update();
+};
 
 const prevHandler = () => {
-idx--;
-const element = numbers[idx];
-element.classList.remove('active')
-console.log(element)
-console.log(idx)
+	element--;
+	update();
+};
+
+function update()  {
+	if(element === 4) { 
+		next.disabled = true;
+	} else {
+		next.disabled = false
+	}
+	if(element === 1) {
+		prev.disabled = true;
+	}else {
+		prev.disabled = false;
+	}
+ numbers.forEach((el,idx)=> {
+	 if(idx < element) {
+		el.classList.add('active');
+	 } else {
+		el.classList.remove('active');
+	 }
+ })
+ const active = document.querySelectorAll('.active');
+ line.style.width = ((active.length-1) / (numbers.length-1)) * 100 +'%';
+ console.log(line.style.width);
 }
-
-
-
 
 next.addEventListener('click', nextHandler);
 prev.addEventListener('click', prevHandler);
